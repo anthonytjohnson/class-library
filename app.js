dcb21dbb8120a8f23ac6230fe6bafd8f917bb951
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-08 13:58:41 +0900

    created subclass Movie

--- app.js
+++ app.js
@@ -51,3 +51,19 @@ class Book extends Media {
     return this._pages;
   }
 }
+
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+}
