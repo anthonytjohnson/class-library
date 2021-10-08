5b917bf908bc0f89fa44514a9a4876b276325cf1
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-08 14:16:28 +0900

    added Todos

--- app.js
+++ app.js
@@ -84,3 +84,11 @@ speed.addRating(1);
 speed.addRating(1);
 speed.addRating(5);
 console.log(speed.getAverageRating());
+
+
+// Add more properties to each class (movieCast, songTitles, etc.)
+// Create a CD class that extends Media.
+// In .addRating(), make sure input is between 1 and 5.
+// Create a method called shuffle for the CD class. The method returns a randomly sorted array of all the songs in the songs property.
+// Create class called Catalog that holds all of the Media items in our library.
+
