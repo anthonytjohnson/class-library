d36d4e2cdc33b22072158fb9199f79d8e8c28487
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-08 13:51:11 +0900

    created parent class Media

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 class Media {
-  
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = {};
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+
+  get ratings() {
+    return this._ratings;
+  }
+
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+  }
+
+  toggleCheckOutStatus() {
+    this.isCheckedOut = !this.isCheckedOut;
+  }
+
+  getAverageRating() {
+    let ratingsSum = this._ratings.reduce((acc, rating) => acc + rating, 0);
+    return ratingsSum / this._ratings.length;
+  }
+
+  addRating(rating) {
+    this._ratings.push(rating);
+    return this._ratings;
+  }
 }
